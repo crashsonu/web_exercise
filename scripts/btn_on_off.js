6e6bc3f280f7db8edecfe8c06350946c5128a0b5
crashsonu <crashsonu@gmail.com>
2019-07-27 17:24:25 +0530

    bulb on off added

---
+++ scripts/btn_on_off.js
@@ -0,0 +1,8 @@
+var btn_on_off_func = function(my_id){
+  if(document.getElementById(my_id).innerHTML=="Turn On"){
+    document.getElementById(my_id).innerHTML="Turn Off"
+  }
+  else{
+    document.getElementById(my_id).innerHTML="Turn On"
+  }
+}
